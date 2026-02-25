8854f0652de6539488dfbcf2ec3b119b511ceeab
RuzoE <eg1853716@gmail.com>
2026-02-25 18:59:18 -0500

    chore: remove compiled blade views from repo and update .gitignore

--- storage/framework/views/c09ab13dc115efd0fa9f0d51a971abc3.php
+++
@@ -1,8 +0,0 @@
-<?php if( session('message') ): ?>
-    <div class="alert-message alert alert-success alert-dismissible fade show" role="alert">
-        <?php echo e(session('message')); ?>
-
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-<?php endif; ?>
-<?php /**PATH C:\laragon\www\hotel-piloto-sam\resources\views/components/show-success.blade.php ENDPATH**/ ?>
